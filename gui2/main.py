f3963f833c616c36960ed6c1863f6616c52510e2
Adog64 <theelectriczlime@gmail.com>
2021-04-06 22:07:38 -0400

    rounded rectangle and time box

--- gui2/main.py
+++ gui2/main.py
@@ -2,7 +2,9 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from backends.util import ScrambleGenerator, SessionManager, LotusTimeManager
+from kivy.core.window import Window
 from kivy.config import Config
+from gui.cwidgets.roundedrectangle import RoundedRectangle
 import os
 
 
@@ -25,7 +27,7 @@ class TimerScreen(Widget):
     time = ObjectProperty(None)
     scramble = ObjectProperty(None)
 
-    def __init__(self, assets):
+    def __init__(self):
         super().__init__()
         self.generate_scramble()
         self.time.bind(text=self.valid_time_input)
@@ -34,8 +36,9 @@ class TimerScreen(Widget):
         self.scramble.text = ScrambleGenerator().generate_scramble()
 
     def enter_time(self):
-        self.scramble.text = ScrambleGenerator().generate_scramble()
-        self.time.text = ''
+        if ltm.is_valid_time(self.time.text):
+            self.scramble.text = ScrambleGenerator().generate_scramble()
+            self.time.text = ''
         self.time.focus = True
 
     def get_time_input(self):
@@ -46,17 +49,13 @@ class TimerScreen(Widget):
 
 class LotusTimer(App):
     def build(self):
-        Config.set('graphics', 'width', '1280')
-        Config.set('graphics', 'height', '720')
         self.title = 'Lotus Timer'
-        self.appdata = os.getenv('APPDATA') + '\\.lotus'
-        self.assets = self.appdata + '\\assets'
-        try:
-            open(self.appdata + '\\readme.txt')
-        except:
-            print('essential files missing or corrupted')
-
-        return TimerScreen(self.assets)
+        Window.size = (1280, 720)
+        Window.resizeable = True
+        Window.icon = assets + 'window_icon.png'
+        #Window.borderless = True
+        Config.set('kivy', 'exit_on_escape', 0)
+        return TimerScreen()
 
 if __name__ == '__main__':
     LotusTimer().run()
